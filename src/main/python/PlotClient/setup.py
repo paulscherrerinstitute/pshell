2e6755fc3441300617c4b95a9cade5788f7f29dd
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2018-02-09 16:48:41 +0100

    Fix test code

--- src/main/python/PlotClient/setup.py
+++ src/main/python/PlotClient/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pplot',
-    version="1.0.0",
+    version="1.0.1",
     description="pplot is Python a client to PShell plot server",
     author='Paul Scherrer Institute',
     requires=["zmq"],
