92e93d3d7578d61e95fb42baaa97c0742f7c4177
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2018-03-23 17:53:49 +0100

    StripScan: manual scan sampling single source, and StripScanExecutor managing multiple independent Strip scans persisted together.

--- src/main/java/ch/psi/pshell/data/LayoutSF.java
+++ src/main/java/ch/psi/pshell/data/LayoutSF.java
@@ -25,6 +25,7 @@ import ch.psi.pshell.scan.RegionScan;
 import ch.psi.pshell.scan.Scan;
 import ch.psi.pshell.scan.ScanRecord;
 import ch.psi.pshell.scan.Search;
+import ch.psi.pshell.scan.StripScan;
 import ch.psi.pshell.scan.TimeScan;
 import ch.psi.pshell.scan.VectorScan;
 import ch.psi.pshell.ui.App;
@@ -96,6 +97,7 @@ public class LayoutSF extends LayoutBase implements Layout {
     public static final String METHOD_CSCAN = "Continuous";
     public static final String METHOD_HSCAN = "OTF";
     public static final String METHOD_BSCAN = "Stream Series";
+    public static final String METHOD_SSCAN = "Strip Series";
     public static final String METHOD_BSEARCH = "Binary Search";
     public static final String METHOD_HSEARCH = "Hill Climbing";
     public static final String METHOD_OPTIMIZATION = "Optimization";
@@ -120,6 +122,8 @@ public class LayoutSF extends LayoutBase implements Layout {
             return METHOD_HSCAN;
         } else if (scan instanceof BsScan) {
             return METHOD_BSCAN;
+        } else if (scan instanceof StripScan) {
+            return METHOD_SSCAN;
         } else if (scan instanceof BinarySearch) {
             return METHOD_BSEARCH;
         } else if (scan instanceof HillClimbingSearch) {
---
+++ src/main/java/ch/psi/pshell/scan/StripScanExecutor.java
@@ -0,0 +1,91 @@
+package ch.psi.pshell.scan;
+
+import ch.psi.pshell.core.Context;
+import ch.psi.pshell.swing.StripChart;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class StripScanExecutor {
+    ExecutorService persistenceExecutor;
+    Thread persistenceThread;    
+    final Map<String, StripScan> scans = new HashMap<>();
+    
+    
+    public void start(String path, String[] ids, String provider, String layout, boolean flush) {
+        persistenceExecutor = Executors.newSingleThreadExecutor((Runnable runnable) -> {
+            persistenceThread = new Thread(Thread.currentThread().getThreadGroup(), runnable, "StripChart Persistence Thread");
+            return persistenceThread;
+        });
+        persistenceExecutor.submit(() -> {
+            Context.getInstance().createExecutionContext();
+
+            scans.clear();
+
+            for (String name : ids) {
+                StripScan scan = new StripScan("Value");
+                Map pars = new HashMap();
+                pars.put("plot_disabled", true);
+                pars.put("table_disabled", true);
+                pars.put("print_scan", false);
+                pars.put("flush", flush);
+                pars.put("path", Context.getInstance().getSetup().expandPath(path));
+                pars.put("layout", layout);
+                pars.put("provider", provider);
+                pars.put("persist", true);
+                pars.put("accumulate", false);
+                pars.put("preserve", false); //Do not now types beforehand, cannot be true
+                pars.put("reset", true);
+                pars.put("tag", name);
+                scan.setHidden(true);
+                Context.getInstance().setCommandPars(scan, pars);
+                scans.put(name, scan);
+                try {
+                    scan.start();
+                } catch (Exception ex) {
+                    Logger.getLogger(StripChart.class.getName()).log(Level.WARNING, null, ex);
+                }
+            }
+        });
+    }   
+    
+    public void append(String id, Double val, long now, long time) {
+        persistenceExecutor.submit(() -> {
+            StripScan scan = scans.get(id);
+            if (scan!=null){
+                scan.append(val, now, time);
+            }
+        });
+    }
+
+    public void finish() {
+        if (!scans.isEmpty()) {
+            Future future = persistenceExecutor.submit(() -> {
+                    for (StripScan scan : scans.values()) {
+                        try {
+                            scan.end();
+                        } catch (Exception ex) {
+                            Logger.getLogger(StripChart.class.getName()).log(Level.WARNING, null, ex);
+                        }
+                    }
+                    Context.getInstance().disposeExecutionContext();
+                });
+
+            scans.clear();
+            persistenceExecutor.shutdown();
+            //try {
+            //   future.get();
+            //} catch (Exception ex) {
+            //    Logger.getLogger(StripChart.class.getName()).log(Level.WARNING, null, ex);
+            //}            
+            persistenceExecutor = null;
+        }
+    }    
+}
