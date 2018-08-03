e00978eb2607f987ec140a90d9a2b81ed9515e67
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2018-08-03 15:22:29 +0200

    Updated Python client version

--- src/main/python/PShellClient/setup.py
+++ src/main/python/PShellClient/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pshell_client',
-    version="1.0.1",
+    version="1.0.2",
     description="pshell_client is Python a client to PShell REST interface",
     author='Paul Scherrer Institute',
     requires=["requests"],
