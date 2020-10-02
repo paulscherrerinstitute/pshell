d11d8ac5bc333fcec058b566a004d0705b55eb23
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2020-10-02 09:29:21 +0200

    Improvements on rsync script

--- src/main/assembly/script/Lib/rsync.py
+++ src/main/assembly/script/Lib/rsync.py
@@ -2,7 +2,6 @@
 #  Utilities for synchronizing folders with rsync
 ####################################################################################################
 
-import subprocess
 import sys
 import os
 import os.path
@@ -18,56 +17,69 @@ def rsync(src, dest, key):
     lines = ret.split("\n")
     lines =  filter(lambda x: x != "", lines)
     if len(lines)<3:
+        print "Invalid return from rsync:\n", ret
         raise Exception ("Invalid format")
     files = lines[1:-2]
 
-    stats = lines[-2]
-    stats = [int(s) for s in stats.split() if s.isdigit()]
-    bytes_sent, bytes_received = stats[0], stats[1]
+    try:
+        stats = lines[-2].replace(",", "").replace(".", "")
+        stats = [int(s) for s in stats.split() if s.isdigit()]
+        bytes_sent, bytes_received = stats[0], stats[1]
+    except:
+        print "Invalid statistics from rsync:\n", ret
+        bytes_sent, bytes_received = None, None
 
     return files, bytes_sent, bytes_received
 
-def sync_user_data(user, src, dest):
+def sync_user_data(user, src, dest, do_log=True, do_print=True):
     try:
+        if do_log:
+            log("Start synchronizing %s to %s:%s" % (src, user, dest), False )
         key = os.path.expanduser("~/.ssh/" + ("ke" if RSYNC_GENERATE_USER_KEY else "id_rsa"))
         if not os.path.isfile(key):
             raise Exception ("Invalid key file")
         dest = user + "@localhost:" + dest
         files, bytes_sent, bytes_received = rsync(src,dest,key)
-        msg = "Transferred " + str(bytes_sent) + " byte to " + user + ": "
+        msg = "Transferred " + str(bytes_sent) + " bytes to " + user + ": "
         for f in files:
             msg = msg + "\n" + f
-        log(msg, False)
-        print msg
+        if do_log:
+            log(msg, False)
+        if do_print:
+            print msg
     except:
         msg = "Error transferring user data to " + user + ": " + str(sys.exc_info()[1])
-        log(msg, False)
-        print >> sys.stderr, msg
+        if do_log:
+            log(msg, False)
+        if do_print:
+            print >> sys.stderr, msg
+    return msg
 
-def remove_user_key():
+def remove_user_key(do_print=True):
     cmd = "rm ~/.ssh/ke;"
     cmd = cmd + "rm ~/.ssh/ke.pub"
-    print exec_cmd(cmd, False)
+    if do_print:
+        print exec_cmd(cmd, False)
 
-def reset_user_key():
-    remove_user_key()
+def reset_user_key(do_print=True):
+    remove_user_key(do_print)
     cmd = "ssh-keygen -N '' -f ~/.ssh/ke -t rsa;"
-    print exec_cmd(cmd)
+    if do_print:
+        print exec_cmd(cmd)
 
-def authorize_user(user):
-
-    aux_file = os.path.expanduser("~/.rsync.tmp")
+def authorize_user(user, aux_file = os.path.expanduser("~/.rsync.tmp"), fix_permissions=True, do_print=True):
     if (os.path.isfile(aux_file)):
         os.remove(aux_file)
     with open(aux_file, "w") as fh:
-        fh.write("Invalid user password")
+        fh.write("Cannot access file: " + aux_file)
     os.chmod(aux_file, 0o777)
 
     success_msg = 'Success transfering authorization key for: ' + user
     cmd =  'echo Authorizing: ' + user + ";"
+    cmd = cmd + 'echo Invalid user or password > ' + aux_file + ";"
     cmd = cmd + "export PK_SUCCESS=FAILURE;"
     if RSYNC_GENERATE_USER_KEY:
-        reset_user_key()
+        reset_user_key(do_print)
         cmd = cmd + "export PK=`cat ~/.ssh/ke.pub`;"
     else:
         cmd = cmd + "export PK=`cat ~/.ssh/id_rsa.pub`;"
@@ -75,6 +87,8 @@ def authorize_user(user):
     cmd = cmd + 'echo $PK >> .ssh/authorized_keys;'
     #cmd = cmd + 'sort .ssh/authorized_keys | uniq > .ssh/authorized_keys.uniq;'
     #cmd = cmd + 'mv .ssh/authorized_keys.uniq .ssh/authorized_keys;'
+    if fix_permissions:
+        cmd = cmd + 'chmod g-w ~' + ";"
     cmd = cmd + 'echo ' + success_msg + ";"
     cmd = cmd + 'echo ' + success_msg + " > " + aux_file + ";"
     cmd = cmd + '"'
@@ -89,7 +103,7 @@ def authorize_user(user):
             raise Exception (ret)
     except:
         if RSYNC_GENERATE_USER_KEY:
-            remove_user_key()
+            remove_user_key(do_print)
         raise  Exception ("Error authenticating user: " + str(sys.exc_info()[1]))
     finally:
         if (os.path.isfile(aux_file)):
