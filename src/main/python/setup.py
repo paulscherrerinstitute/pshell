efc45c37b2533013192a6ebe619f50bc00cdeace
gobbo_a <alexandre.gobbo@psi.ch>
2022-09-30 14:29:20 +0200

    Version 1.19-SNAPSHOT

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pshell',
-    version="1.18.0",
+    version="1.19.0",
     description="pshell is Python a client to PShell REST interface",
     author='Paul Scherrer Institute',
     requires=["requests"],
