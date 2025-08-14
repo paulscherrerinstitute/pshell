3aa89651c1e46ad8aa65c2be5e5727379ef3b228
gobbo_a <alexandre.gobbo@psi.ch>
2025-08-14 15:46:21 +0200

    Remove redundancy

--- workbench/src/main/java/ch/psi/pshell/workbench/Options.java
+++ workbench/src/main/java/ch/psi/pshell/workbench/Options.java
@@ -9,15 +9,10 @@ import ch.psi.pshell.app.Option;
  */
  public enum Options implements Option {
     PREFERENCES,
-    STDIO;
-    
-    public static void addShell(){     
-        ch.psi.pshell.framework.Options.add();
-        ch.psi.pshell.app.Options.CONFIG.add("cfg", "Set config file (default is " + ch.psi.pshell.app.Options.CONFIG.toEnvVar() + " or else {config}/config.properties)", "path");
-    }    
+    STDIO; 
     
     public static void add(){           
-        addShell();
+        ch.psi.pshell.console.Options.add();
         ch.psi.pshell.xscan.Options.addSpecific();
         STDIO.add("dual", "Start GUI command line interface (not allowed if running in the background)");                        
         PREFERENCES.add("pref", "Override the view preferences file", "path");                
